fd1bc7df4ce5b103dee0b20b922dc3bad5b3d3dd
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-08-31 20:30:23 +0530

    Create start.py

---
+++ Waifu/Plugins/start.py
@@ -0,0 +1,18 @@
+import asyncio
+from Waifu.database.main import add_users_to_db, get_users_list
+from pyrogram import filters, enums 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from Waifu import waifu
+from Waifu import prefix
+
+@bot.on_message(filters.command("start", prefix) | filters.private)
+async def start(_, message):
+    user_id = int(message.from_user.id)
+    mention = message.from_user.mention
+
+    if message.chat.type == enums.ChatType.PRIVATE:
+        # Check if the user is already in the database
+        if user_id not in await get_users_list():
+            await add_users_to_db(user_id)
+
+
