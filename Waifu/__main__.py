a3585a0be965eb70ed7a3e948895d46f74e7f2ef
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-08-31 19:49:01 +0530

    Update __main__.py

--- Waifu/__main__.py
+++ Waifu/__main__.py
@@ -3,11 +3,11 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Waifu import waifu
 
 markup = InlineKeyboardMarkup([
-    [InlineKeyboardButton("Oᗯᑎᗴᖇ", url="https://t.me/just_a_bio"),
-     InlineKeyboardButton("ՏᑌᑭᑭOᖇT", url="https://t.me/just_a_bio")]
+    [InlineKeyboardButton("OWNER", url="https://t.me/just_a_bio"),
+     InlineKeyboardButton("SUPPORT", url="https://t.me/just_a_bio")]
 ])
 
-caption = "YᗩI ᗰIKO ՏTᗩᖇTᗴᗪ ՏᑌᑕᑕᗴՏՏᖴᑌᒪᒪY"
+caption = "TEAM-X WAIFU GRABBER STARTED"
 photo = "https://telegra.ph/file/dc35ba52828e09744b3cc.jpg"
 GROUP = "-1001964645198"
 
