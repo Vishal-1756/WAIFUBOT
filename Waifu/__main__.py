29f7ba0d756efa2ad00fe01d7225a7d6bce46e4e
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-09-01 21:24:58 +0530

    Update __main__.py

--- Waifu/__main__.py
+++ Waifu/__main__.py
@@ -13,4 +13,4 @@ photo = "https://telegra.ph/file/dc35ba52828e09744b3cc.jpg"
 if __name__ == "__main__":
     waifu.run()
     with waifu:
-        waifu.send_photo(chat_id=-1001849819947, photo=photo, caption=caption, reply_markup=markup)
+        waifu.send_photo(chat_id=-1001886896149, photo=photo, caption=caption, reply_markup=markup)
