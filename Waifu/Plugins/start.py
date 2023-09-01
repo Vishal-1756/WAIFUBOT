0c3a03e26598cea049098780f99084c219bae7e3
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-09-01 21:37:24 +0530

    Update start.py

--- Waifu/Plugins/start.py
+++ Waifu/Plugins/start.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Waifu import waifu
 from Waifu import prefix
 
-@bot.on_message(filters.command("start", prefix) | filters.private)
+@waifu.on_message(filters.command("start", prefix) | filters.private)
 async def start(_, message):
     user_id = int(message.from_user.id)
     mention = message.from_user.mention
