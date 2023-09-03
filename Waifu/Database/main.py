a7ae8506596c4c3371f1df9911c94fb6a828e663
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-09-03 09:21:22 +0530

    Update main.py

--- Waifu/Database/main.py
+++ Waifu/Database/main.py
@@ -8,7 +8,7 @@ async def add_users_to_db(user_id: int):
 
 async def get_users_list():
      list = [x["user_id"] for x in db.find()]
-     return lis
+     return list
 
 async def add_waifu_to_db(user_id: int, waifu_name: str):
     user_data = db.find_one({"user_id": user_id})
