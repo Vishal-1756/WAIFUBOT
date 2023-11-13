61b3faf8c01dc83220db2a37e2d1da81b90fac39
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-13 22:14:13 +0530

    Update __main__.py

--- Waifu/__main__.py
+++ Waifu/__main__.py
@@ -3,14 +3,14 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Waifu import waifu
 
 markup = InlineKeyboardMarkup([
-    [InlineKeyboardButton("OWNER", url="https://t.me/just_a_bio"),
-     InlineKeyboardButton("SUPPORT", url="https://t.me/just_a_bio")]
+    [InlineKeyboardButton("OWNER", url="https://t.me/IkariS0_0"),
+     InlineKeyboardButton("SUPPORT", url="https://t.me/IkariS0_0")]
 ])
 
-caption = "TEAM-X WAIFU GRABBER STARTED"
-photo = "https://telegra.ph/file/dc35ba52828e09744b3cc.jpg"
+caption = "WAIFU ADDER STARTED ADD WAIFUS IN DB"
+photo = "https://telegra.ph/file/aa68b5d8185a9d5cadf63.jpg"
 
 if __name__ == "__main__":
     waifu.run()
     with waifu:
-        waifu.send_photo(chat_id=-1001964645198, photo=photo, caption=caption, reply_markup=markup)
+        waifu.send_photo(chat_id=-1001849819947, photo=photo, caption=caption, reply_markup=markup)
