4b4fc2d56ec464913192c1790f7d6afc0b415c8e
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-13 22:11:25 +0530

    Update and rename add.. to add.py

--- Waifu/Plugins/add.py
+++ Waifu/Plugins/add.py
@@ -6,12 +6,12 @@ from Waifu import waifu
 
 db = DATABASE["MAIN"]
 
-
-def insert_waifu_data(waifu_name, rank, image_url):
+def insert_waifu_data(waifu_name, rarity, image_url, source):
     waifu_data = {
         "waifu_name": waifu_name,
-        "rank": rank,
-        "image_url": image_url
+        "rarity": rarity,
+        "image_url": image_url,
+        "source": source
     }
     db.insert_one(waifu_data)
 
@@ -23,13 +23,13 @@ async def add_new_waifu_command(_, message):
 
     # Parse the input for waifu data
     try:
-        waifu_name, rank, image_url = input_text.split("-", 2)
+        waifu_name, rarity, image_url, source = input_text.split("-", 3)
     except ValueError:
-        await message.reply("Invalid format. Use /addwaifu waifuname-rank-imageurl")
+        await message.reply("Invalid format. Use /addwaifu waifuname-rarity-imageurl-source")
         return
 
     # Insert waifu data into the database
-    insert_waifu_data(waifu_name, rank, image_url)
+    insert_waifu_data(waifu_name, rarity, image_url, source)
     await message.reply("Waifu data added successfully!")
 
 # Command to fetch waifu data
@@ -38,13 +38,13 @@ async def fetch_waifu_data(_, message):
     waifus = db.find()  # Retrieve all waifu data from the database
 
     for waifu in waifus:
-        if "waifu_name" in waifu and "rank" in waifu and "image_url" in waifu:
+        if "waifu_name" in waifu and "rarity" in waifu and "image_url" in waifu and "source" in waifu:
             waifu_name = waifu["waifu_name"]
-            rank = waifu["rank"]
+            rarity = waifu["rarity"]
             image_url = waifu["image_url"]
+            source = waifu["source"]
 
-            caption = f"Name: {waifu_name}\nRank: {rank}"
+            caption = f"Name: {waifu_name}\nRarity: {rarity}\nSource: {source}"
             await message.reply_photo(photo=image_url, caption=caption)
         else:
             await message.reply("Waifu data is missing some fields and cannot be displayed.")
-
