1b609a7b47f292b8aafdedf0a7f2cf44fcc9221e
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-14 12:14:20 +0530

    Update chatbot.py

--- Waifu/Plugins/chatbot.py
+++ Waifu/Plugins/chatbot.py
@@ -6,24 +6,31 @@ import requests
 from pyrogram import filters, Client
 from pyrogram.types import Message
 from pyrogram.enums import ChatType
+import json
 from Waifu import waifu as pbot
 
-@pbot.on_message(~filters.bot & ~filters.me & filters.text)
-async def chatbot(_:Client,message:Message):
-    if message.chat.type!= ChatType.PRIVATE:
+@pbot.on_message(filters.text & ~filters.bot & ~filters.me)
+async def chatbot(_, message: Message):
+    if message.chat.type != ChatType.PRIVATE:
         if not message.reply_to_message:
             return
         if message.reply_to_message.from_user.id != (await pbot.get_me()).id:
             return
+
     if message.text and message.text[0] in ["/", "!", "?", "."]:
         return
-    response = requests.get("https://pervert-api.onrender.com/chatbot/{message.text}")
+
+    response = requests.get(f"https://pervert-api.onrender.com/chatbot/{message.text}")
+
     if response.status_code == 200:
-        data = response.json()['response']
-        return await message.reply_text(data)
+        try:
+            results = response.json()
+            await message.reply_text(results.get("reply", "No reply from the chatbot."))
+        except json.JSONDecodeError:
+            await message.reply_text("Failed to decode the chatbot response.")
     elif response.status_code == 429:
-        return await message.reply_text("ChatBot Error: Too many requests. Please wait a few moments.")
+        await message.reply_text("ChatBot Error: Too many requests. Please wait a few moments.")
     elif response.status_code >= 500:
-        return await message.reply_text("ChatBot Error: API server error. Contact us at @Ikaris0_0.")
+        await message.reply_text("ChatBot Error: API server error. Contact us at @Ikaris0_0.")
     else:
-        return await message.reply_text("ChatBot Error: Unknown Error Occurred. Contact us at @Ikaris0_0.")
+        await message.reply_text(f"ChatBot Error: Unknown Error Occurred. Contact us at @Ikaris0_0.")
