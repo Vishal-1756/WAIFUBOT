42ad5bfeafdd6ffcd8979ad5fc6581b22ee5b8f0
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-14 10:07:06 +0530

    Update spawn.py

--- Waifu/Plugins/spawn.py
+++ Waifu/Plugins/spawn.py
@@ -1,5 +1,5 @@
 import random
-from pyrogram import filters, Client
+from pyrogram import filters
 from pyrogram.types import Message
 from Waifu.Database.main import add_waifu_to_db
 from Waifu import waifu, prefix
@@ -14,10 +14,13 @@ spawned_waifu_name = None
 # Counter to track the number of messages in the group
 message_count = 0
 
+# Global variable to store the spawned waifu
+spawned_waifu = None
+
 # Your message handler for text messages in groups
 @waifu.on_message(filters.text & filters.group)
 async def on_text_message(_, message: Message):
-    global message_count, spawned_waifu_name
+    global message_count, spawned_waifu_name, spawned_waifu
 
     message_count += 1
 
@@ -25,7 +28,7 @@ async def on_text_message(_, message: Message):
         message_count = 0
 
         # Retrieve a random waifu from the database
-        random_waifu = collection.aggregate([{ "$sample": { "size": 1 } } ])
+        random_waifu = collection.aggregate([{ "$sample": { "size": 1 } }])
         spawned_waifu = next(random_waifu, None)
 
         if spawned_waifu:
@@ -41,11 +44,10 @@ async def on_text_message(_, message: Message):
         else:
             await waifu.send_message(chat_id=message.chat.id, text="No waifus found in the database.")
 
-
 # Your message handler for the /catch command
 @waifu.on_message(filters.command("catch", prefix) & filters.group)
 async def catch_waifu(_, message):
-    global spawned_waifu_name
+    global spawned_waifu_name, spawned_waifu
 
     if spawned_waifu_name:
         query = message.text.split(maxsplit=1)[1]
@@ -72,4 +74,3 @@ async def catch_waifu(_, message):
             await waifu.send_message(chat_id=message.chat.id, text=f"You missed! The spawned waifu's name is {spawned_waifu_name}. Try again.")
     else:
         await waifu.send_message(chat_id=message.chat.id, text="No waifu to catch. Wait for a spawned waifu.")
-        
