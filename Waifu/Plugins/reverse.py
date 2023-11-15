e0521e581bc5eca1a353158ad759ac62f77a4ed6
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-15 15:35:22 +0530

    Update reverse.py

--- Waifu/Plugins/reverse.py
+++ Waifu/Plugins/reverse.py
@@ -8,26 +8,33 @@ from unidecode import unidecode
 from pyrogram.enums import *
 from pyrogram.types import *
 
-async def Sauce(bot_token,file_id):
+async def Sauce(bot_token, file_id):
     r = requests.post(f'https://api.telegram.org/bot{bot_token}/getFile?file_id={file_id}').json()
     file_path = r['result']['file_path']
     headers = {'User-agent': 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'}
     to_parse = f"https://images.google.com/searchbyimage?safe=off&sbisrc=tg&image_url=https://api.telegram.org/file/bot{bot_token}/{file_path}"
-    r = requests.get(to_parse,headers=headers)
+    r = requests.get(to_parse, headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
-    result = {                            
-             "similar": '',
-             'output': ''
-         }
-    for similar_image in soup.find_all('input', {'class': 'gLFyf'}):
-         url = f"https://www.google.com/search?tbm=isch&q={quote_plus(similar_image.get('value'))}"
-         result['similar'] = url
-    for best in soup.find_all('div', {'class': 'r5a77d'}):
-        output = best.get_text()
-        decoded_text =  unidecode(output)
-        result["output"] = decoded_text
-        
+
+    result = {
+        "similar": '',
+        'output': ''
+    }
+
+    # Extracting the best guess
+    best_guess = soup.find('a', {'class': 'BVG0Nb'})
+    if best_guess:
+        result['output'] = best_guess.text.strip()
+
+    # Extracting the similar images link
+    search_by_image_div = soup.find('div', {'class': 'r5a77d'})
+    if search_by_image_div:
+        search_by_image_link = search_by_image_div.find('a', recursive=False)
+        if search_by_image_link and search_by_image_link.get('href'):
+            result['similar'] = 'https://www.google.com' + search_by_image_link['href']
+
     return result
+
 
 async def get_file_id_from_message(msg):
     file_id = None
