b12158e2f32a7dc54623a0995a17cf026409d210
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-15 15:05:42 +0530

    Update reverse.py

--- Waifu/Plugins/reverse.py
+++ Waifu/Plugins/reverse.py
@@ -1,78 +1,22 @@
-import requests
-from pyrogram import filters, Client
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from Waifu import waifu as pbot
-from Waifu import bot_token as TOKEN
+# BSD 3-Clause License
+# Copyright (c) 2023, Yash-Sharma-1807
 
-API_URL = "https://gglimg.vercel.app/reverse"
+import os
+from Waifu import waifu as app
+from pyrogram import *
+from pyrogram.types import *
+from datetime import datetime
+from GoogleSearch import Search
 
-async def get_file_id_from_message(message: Message):
-    file_id = None
-    message = message.reply_to_message
-    if not message:
-        return None
-    if message.document:
-        if int(message.document.file_size) > 3145728:
-            return
-        mime_type = message.document.mime_type
-        if mime_type not in ("image/png", "image/jpeg"):
-            return
-        file_id = message.document.file_id
-
-    if message.sticker:
-        if message.sticker.is_animated:
-            if not message.sticker.thumbs:
-                return
-            file_id = message.sticker.thumbs[0].file_id
-        else:
-            file_id = message.sticker.file_id
-
-    if message.photo:
-        file_id = message.photo.file_id
-
-    if message.animation:
-        if not message.animation.thumbs:
-            return
-        file_id = message.animation.thumbs[0].file_id
-
-    if message.video:
-        if not message.video.thumbs:
-            return
-        file_id = message.video.thumbs[0].file_id
-    return file_id
-
-@pbot.on_message(filters.command(["pp", "grs", "reverse"]))
-async def reverse(client: Client, message: Message):
-    text = await message.reply_text("`Parsing Media...`")  # Simple text
-    file_id = await get_file_id_from_message(message)
-    if not file_id:
-        return await text.edit("`Reply to a Photo or sticker`")  # Simple text
-    await text.edit("`Searching...`")  # Simple text
-
-    r = requests.post(
-        f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}"
-    ).json()
-    file_path = r["result"]["file_path"]
-
-    data = {
-        "imageUrl": f"https://images.google.com/searchbyimage?safe=off&sbisrc=tg&image_url=https://api.telegram.org/file/bot{TOKEN}/{file_path}"
-    }
-
-    response = requests.post(API_URL, json=data)
-
-    if response.status_code == 200:
-        result = response.json()["data"]
-        return await text.edit(
-            f'Sauce: {result["output"]}',  # Simple text
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Link", url=result["similar"])]]
-            ),
-        )
-    elif response.status_code == 401:
-        return await text.edit("`Couldn't find anything`")  # Simple text
-    elif response.status_code == 402:
-        return await text.edit("`Failed to reverse image`")  # Simple text
-    elif response.status_code <= 500:
-        return await text.edit("`Error in API`")  # Simple text
-    else:
-        return await text.edit("`Unknown Error Occurred`")  # Simple text
+@app.on_message(filters.command("pp") & filters.group, group=3)
+async def pp(_,msg:Message):
+    "reverse the image and send results"
+    now = datetime.utcnow()
+    if msg.reply_to_message and msg.reply_to_message.photo:
+        ms = await msg.reply_text("wait for 2-3 second")
+        X = await app.download_media(msg.reply_to_message,f"{now.microsecond}.jpg")
+        sea = Search(X)
+        await ms.edit_text(f"[{sea['output']}]({sea['similar']})")
+        os.remove(f"./downloads/{now.microsecond}.jpg")
+    else :
+        await msg.reply("Reply to a image")
