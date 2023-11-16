0ffdf9ecac6b6a9a840ce4a4838c3e56c369cc28
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-16 07:09:01 +0530

    Update reverse.py

--- Waifu/Plugins/reverse.py
+++ Waifu/Plugins/reverse.py
@@ -14,7 +14,7 @@ async def Sauce(bot_token, file_id):
     headers = {'User-agent': 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'}
     to_parse = f"https://images.google.com/searchbyimage?safe=off&sbisrc=tg&image_url={quote_plus(f'https://api.telegram.org/file/bot{bot_token}/{file_path}')}"
 
-    r = requests.get(encoded_to_parse, headers=headers)
+    r = requests.get(to_parse, headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
     
     result = {
