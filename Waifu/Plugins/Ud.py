753b6aeb74141cacfd8aa9c9f0374ec64e4f5a1b
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-20 10:21:40 +0530

    Update Ud.py

--- Waifu/Plugins/Ud.py
+++ Waifu/Plugins/Ud.py
@@ -1,3 +1,7 @@
+# Made By @Ikaris0_0
+# Join Support:- @botsupportx
+# Api By @Ikaris0_0
+
 import requests
 from pyrogram import filters
 from pyrogram.types import Message
@@ -6,27 +10,27 @@ from pyrogram.enums import ParseMode
 
 @waifu.on_message(filters.command("ud", prefix))
 async def ud(_, message: Message):
-    # Extract the word to define from the user's message
+    
     text = message.text.split(maxsplit=1)[1] if len(message.text.split()) > 1 else None
 
     if not text:
         await message.reply("Please provide a word to define. Example: /ud <word>")
         return
 
-    # Get the definition and examples for the provided word
+    
     response = requests.get(f"https://pervert-api.onrender.com/ud?query={text}&max=2").json()
 
     try:
         results = response.get("results", [])
         if results:
-            # Extracting the first result
+            
             first_result = results[0]
             definition = first_result.get("definition", "")
             example = first_result.get("example", "")
 
-            reply_text = f'*{text}*\n\n{definition}\n\n_{example}_'
+            reply_text = f'**Word**:- **{text}**\n\n **Defination**:- **{definition}**\n\n**Example**:- **{example}**'
         else:
-            reply_text = "No results found."
+            reply_text = f"No Results Found For Word:-{text}"
     except (KeyError, IndexError):
         reply_text = "Error processing the API response."
 
