8c3f0e03e01e0c8bc04f48cf0d4496b4ded5188f
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-20 10:07:57 +0530

    Update Ud.py

--- Waifu/Plugins/Ud.py
+++ Waifu/Plugins/Ud.py
@@ -1,5 +1,5 @@
 import requests
-from pyrogram import filters, enums
+from pyrogram import filters
 from pyrogram.types import Message
 from Waifu import waifu, prefix
 
@@ -9,16 +9,24 @@ async def ud(_, message: Message):
     text = message.text.split(maxsplit=1)[1] if len(message.text.split()) > 1 else None
 
     if not text:
-        await message.reply("Please provide a word to define. Example: /ud ")
+        await message.reply("Please provide a word to define. Example: /ud <word>")
         return
 
     # Get the definition and examples for the provided word
-    results = requests.get(f"https://pervert-api.onrender.com/ud?query={text}&max=2").json()
+    response = requests.get(f"https://pervert-api.onrender.com/ud?query={text}&max=2").json()
 
     try:
-        reply_text = f'*{text}*\n\n{results["definition"]}\n\n_{results["example"]}_'
+        results = response.get("results", [])
+        if results:
+            # Extracting the first result
+            first_result = results[0]
+            definition = first_result.get("definition", "")
+            example = first_result.get("example", "")
+
+            reply_text = f'*{text}*\n\n{definition}\n\n_{example}_'
+        else:
+            reply_text = "No results found."
     except (KeyError, IndexError):
-        reply_text = "No results found."
+        reply_text = "Error processing the API response."
 
-    await message.reply_text(reply_text, parse_mode=enums.ParseMode.MARKDOWN)
-
+    await message.reply_text(reply_text, parse_mode="markdown")
