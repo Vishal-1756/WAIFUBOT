2e9e88aeb5cd55fca452639b54228f6cd9e9e3b4
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-21 20:32:46 +0530

    Update start.py

--- Waifu/Plugins/start.py
+++ Waifu/Plugins/start.py
@@ -2,7 +2,7 @@ import asyncio
 import random
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from Waifu.Database.main import add_users_to_db, get_users_list
+from Waifu.Database.main import add_users_to_db, get_users_list, add_chat_to_db, get_chats_list
 from Waifu import waifu
 from Waifu import prefix
 
@@ -14,30 +14,35 @@ photo_links = [
     "https://telegra.ph/file/3b5ebeeb66bdef64b87fd.jpg"
 ]
 
-@waifu.on_message(filters.command("start", prefix) | filters.private)
+@waifu.on_message(filters.command("start", prefix) | filters.private | filters.group)
 async def start(_, message):
     user_id = int(message.from_user.id)
+    chat_id = int(message.chat.id)
     mention = message.from_user.mention
-    if message.chat.type == enums.ChatType.PRIVATE:
-        # Check if the user is already in the database
-        if user_id not in await get_users_list():
-            await add_users_to_db(user_id)
 
-        reply_markup = InlineKeyboardMarkup(
+    # Check if the user is already in the database
+    if user_id not in await get_users_list():
+        await add_users_to_db(user_id)
+
+    # Add the group to the database if it's not already there
+    if chat_id not in await get_chats_list():
+        await add_chat_to_db(message.chat)
+
+    reply_markup = InlineKeyboardMarkup(
+        [
             [
-                [
-                    InlineKeyboardButton("⤜ Sᴜᴘᴘᴏʀᴛ ⤛", url="https://t.me/botsupportx"),
-                    InlineKeyboardButton("⇜ Uᴘᴅᴀᴛᴇs ⇝", url="https://t.me/botupdatex")
-                ],
-                [
-                    InlineKeyboardButton("☊ Oᴡɴᴇʀ ☋", url="https://t.me/Ikaris0_0"),
-                    InlineKeyboardButton("☌ Cʀᴇᴅɪᴛs ☌", url="https://telegra.ph/𓆩Ꭰᥲʀκ𓆪-𖤍-11-20-2")
-                ],
-                [
-                    InlineKeyboardButton("+ Aᴅᴅ Mᴇ Iɴ Gʀᴏᴜᴘ +", url="https://t.me/Chat_Rank_Roobot?startgroup=true")
-                ]
+                InlineKeyboardButton("⤜ Sᴜᴘᴘᴏʀᴛ ⤛", url="https://t.me/botsupportx"),
+                InlineKeyboardButton("⇜ Uᴘᴅᴀᴛᴇs ⇝", url="https://t.me/botupdatex")
+            ],
+            [
+                InlineKeyboardButton("☊ Oᴡɴᴇʀ ☋", url="https://t.me/Ikaris0_0"),
+                InlineKeyboardButton("☌ Cʀᴇᴅɪᴛs ☌", url="https://telegra.ph/𓆩Ꭰᥲʀκ𓆪-𖤍-11-20-2")
+            ],
+            [
+                InlineKeyboardButton("+ Aᴅᴅ Mᴇ Iɴ Gʀᴏᴜᴘ +", url="https://t.me/Chat_Rank_Roobot?startgroup=true")
             ]
-        )
+        ]
+    )
 
-        photo_link = random.choice(photo_links)
-        await waifu.send_photo(chat_id=message.chat.id, photo=photo_link, caption=start_message.format(mention=mention), reply_markup=reply_markup)
+    photo_link = random.choice(photo_links)
+    await waifu.send_photo(chat_id=message.chat.id, photo=photo_link, caption=start_message.format(mention=mention), reply_markup=reply_markup)
