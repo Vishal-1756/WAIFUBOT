73fc8d1a73713a53b1891eef6cee7b649ccadd98
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-21 21:21:33 +0530

    Create stats.py

---
+++ Waifu/Plugins/stats.py
@@ -0,0 +1,23 @@
+from pyrogram import filters
+from Database.main import get_chats_list, get_users_list
+from Waifu import waifu
+
+@waifu.on_message(filters.command("stats", prefixes="/"))
+async def bot_stats(_, message):
+    total_users = await get_users_count()
+    total_chats = await get_chats_count()
+
+    text = f"<b>Bot Statistics:</b>\n\n"
+    text += f"Total Users: {total_users}\n"
+    text += f"Total Chats: {total_chats}\n"
+
+    await message.reply_text(text, parse_mode="html")
+
+async def get_users_count():
+    user_ids = await get_users_list()
+    return len(user_ids)
+
+async def get_chats_count():
+    chats = await get_chats_list()
+    return len(chats)
+  
