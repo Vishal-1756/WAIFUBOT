ecf28dc2a6d6f1d18c0550d73f21d67f4fe925d9
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-21 21:45:14 +0530

    Update start.py

--- Waifu/Plugins/start.py
+++ Waifu/Plugins/start.py
@@ -14,8 +14,8 @@ photo_links = [
     "https://telegra.ph/file/3b5ebeeb66bdef64b87fd.jpg"
 ]
 
-@waifu.on_message(filters.command("start", prefix))
-async def start(_, message):
+@waifu.on_message(filters.command("start", prefix) & filters.private)
+async def start_private(_, message):
     user_id = int(message.from_user.id)
     mention = message.from_user.mention
 
@@ -36,17 +36,39 @@ async def start(_, message):
     )
 
     photo_link = random.choice(photo_links)
-   
-    if message.chat.type == enums.ChatType.PRIVATE:
-        # Check if the user is already in the database
-        if user_id not in await get_users_list():
-            await add_users_to_db(user_id)
-            
-        
-    elif message.chat.type == enums.ChatType.SUPERGROUP:
-        # Check if the chat is already in the database
-        if message.chat.id not in await get_chats_list():
-            await add_chat_to_db(message.chat)
-    
+
+    # Check if the user is already in the database
+    if user_id not in await get_users_list():
+        await add_users_to_db(user_id)
+
     await waifu.send_photo(chat_id=message.chat.id, photo=photo_link, caption=start_message.format(mention=mention), reply_markup=reply_markup)
 
+
+@waifu.on_message(filters.command("start", prefix) & filters.group)
+async def start_group(_, message):
+    chat_id = int(message.chat.id)
+    mention = message.from_user.mention
+
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("⤜ Sᴜᴘᴘᴏʀᴛ ⤛", url="https://t.me/botsupportx"),
+                InlineKeyboardButton("⇜ UᴘᴅᴀᴛᴇS ⇝", url="https://t.me/botupdatex")
+            ],
+            [
+                InlineKeyboardButton("☊ Oᴡɴᴇʀ ☋", url="https://t.me/Ikaris0_0"),
+                InlineKeyboardButton("☌ Cʀᴇᴅɪᴛs ☌", url="https://telegra.ph/𓆩Ꭰᥲʀκ𓆪-𖤍-11-20-2")
+            ],
+            [
+                InlineKeyboardButton("+ Aᴅᴅ Mᴇ Iɴ Gʀᴏᴜᴘ +", url="https://t.me/Chat_Rank_Roobot?startgroup=true")
+            ]
+        ]
+    )
+
+    photo_link = random.choice(photo_links)
+
+    # Check if the chat is already in the database
+    if chat_id not in await get_chats_list():
+        await add_chat_to_db(message.chat)
+
+    await waifu.send_photo(chat_id=message.chat.id, photo=photo_link, caption=start_message.format(mention=mention), reply_markup=reply_markup)
