8baf27fa2f056ef6b569689aef1562c9d6f45556
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-23 07:24:20 +0530

    Create sauce.py

---
+++ Waifu/Plugins/sauce.py
@@ -0,0 +1,64 @@
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaVideo
+from pyrogram.file_id import FileId
+from io import BytesIO
+import base64
+import mimetypes
+import requests
+import aiohttp
+from Waifu import waifu as app
+
+async def telegraph(attachment_data):
+    form = FormData()
+    form.add_file("file", BytesIO(base64.b64decode(attachment_data['data'])),
+                  filename=f"telegraph.{mimetypes.guess_extension(attachment_data['mimetype'])}")
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post("https://te.legra.ph/upload", data=form) as resp:
+            response_data = await resp.json()
+            return "https://te.legra.ph" + response_data[0]['src']
+
+async def shorten_video_url(video_url):
+    try:
+        response = await requests.get('https://api.erdwpe.com/api/linkshort/tinyurl', params={'link': video_url})
+        data = response.json()
+        if data['status']:
+            return data['result']
+        else:
+            print('TinyURL API request failed:', data)
+            return None
+    except Exception as e:
+        print('Error shortening URL:', e)
+        return None
+
+@app.on_message(filters.command("sauce"))
+async def sauce_command(_, message):
+    try:
+        await message.delete(True)
+        if message.reply_to_message:
+            quoted_msg = message.reply_to_message
+            attachment_data = await app.download_media(quoted_msg)
+            tdata = await telegraph(attachment_data)
+            
+            if tdata == "error":
+                await quoted_msg.reply("Error occurred while creating a direct link.")
+            else:
+                response = await requests.get(f'https://api.betabotz.org/api/webzone/whatanime?query={tdata}&apikey=GK5zaGhL')
+                jdata = response.json()
+                name = jdata['result']['data']['filename'].replace('.mp4', '')
+                episode = jdata['result']['data']['episode']
+                image = jdata['result']['data']['image']
+                video = jdata['result']['data']['video']
+
+                tinyurl = await shorten_video_url(video)
+                caption = f"Name: *{name}*\nEpisode: *{episode}*\nVideo: *{tinyurl}*"
+
+                try:
+                    media = InputMediaVideo(FileId(video))
+                    await app.send_message(message.chat.id, media, caption=caption, parse_mode='Markdown')
+                except Exception as e:
+                    await message.reply("Error while sending video")
+        else:
+            await message.reply("*Error*\n```Please reply to a media file```")
+    except Exception as e:
+        await message.reply("*Error*\n```Can't support text!```")
