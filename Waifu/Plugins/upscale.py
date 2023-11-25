222676b1be5dad19f821e7cda349faad4b5d6112
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-25 17:01:16 +0530

    Update upscale.py

--- Waifu/Plugins/upscale.py
+++ Waifu/Plugins/upscale.py
@@ -5,7 +5,36 @@ import os
 import base64
 from pyrogram import *
 from Waifu import waifu as app
+import asyncio
+from lexica import AsyncClient
+from lexica.constants import languageModels
 
+async def getFile(message):
+    if not message.reply_to_message:
+        return None
+    if message.reply_to_message.document is False or message.reply_to_message.photo is False:
+        return None
+    if message.reply_to_message.document and message.reply_to_message.document.mime_type in ['image/png','image/jpg','image/jpeg'] or message.reply_to_message.photo:
+        image = await message.reply_to_message.download()
+        return image
+    else:
+        return None
+
+
+async def UpscaleImages(image: bytes) -> str:
+    """
+    Upscales an image and return with upscaled image path.
+    """
+    try:
+        client = AsyncClient()
+        content = await client.upscale(image)
+        await client.close()
+        upscaled_file_path = "upscaled.png"
+        with open(upscaled_file_path, "wb") as output_file:
+            output_file.write(content)
+        return upscaled_file_path
+    except Exception as e:
+        raise Exception(f"Failed to upscale the image: {e}")
 
 @app.on_message(filters.command("upscale"))
 async def upscale(client,message):
@@ -23,9 +52,27 @@ async def upscale(client,message):
         resp = requests.get(link)
         data = resp.content
         Img = Image.open(BytesIO(data))
-        Img.save("upscale.png")
+        Imgfrom pyrogram import Client, filters, types as t
+
+@app.on_message(filters.command(["up"]))
+async def upscaleImages(_, message):
+    file = await getFile(message)
+    if file is None:
+        return await message.reply_text("`Dear Pro Ppls Reply To a Media File 🗃️`")
+    msg = await message.reply("`Requesting to api wait...`")
+    imageBytes = open(file,"rb").read()
+    os.remove(file)
+    upscaledImage = await UpscaleImages(imageBytes)
+    try:
+      await message.reply_document(open(upscaledImage,"rb"))
+      await msg.delete()
+      os.remove(upscaledImage)
+    except Exception as e:
+       await msg.edit(f"{e}").save("upscale.png")
         await message.reply_document("upscale.png")
         await temp.delete()
         os.remove("upscale.png")
     except:
         return await temp.edit_text("**Try again after 10 seconds.**")
+
+
