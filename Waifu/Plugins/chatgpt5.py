511b21a2f07bd414f06b55a6d959e6fbe9c6ca2e
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-26 12:05:10 +0530

    Update chatgpt5.py

--- Waifu/Plugins/chatgpt5.py
+++ Waifu/Plugins/chatgpt5.py
@@ -71,3 +71,34 @@ async def chatgpt5(_: Client, message: Message):
     txt = await message.reply_text("Wait patiently, requesting to API...")
     api_response, error_message = fetch_data(api_url_chat5, query)
     await txt.edit(api_response or error_message)
+    
+
+api_url = "https://pervert-api.onrender.com/photoleapai"
+
+async def fetch_data(query: str) -> str:
+    try:
+        response = requests.get(f"{api_url}?query={query}")
+        response.raise_for_status()
+        data = response.json()
+        photo_url = data.get("photoleapai")
+        return photo_url
+    except requests.exceptions.RequestException as e:
+        return f"Request error: {e}"
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+@app.on_message(filters.command("photoleap"))
+async def photoleap(_, message: Message):
+    if len(message.command) < 2:
+        return await message.reply_text("**Please provide a query.**")
+
+    query = " ".join(message.command[1:])
+    photo_url = await fetch_data(query)
+
+    if photo_url.startswith("http"):
+        caption = f"Result for: `{query}`\nSupport: @botsupportx\nUpdates: @botupdatex"
+        await message.reply_photo(photo_url, caption=caption)
+    else:
+        await message.reply_text(photo_url)
+
+    
