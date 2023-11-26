6b103af80e8998aa5fba1e17dd37442bc6c1cdaf
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-26 09:54:54 +0530

    Update and rename Waifu/Plugins/Harem to chatgpt5.py

---
+++ chatgpt5.py
@@ -0,0 +1,18 @@
+from pyrogram import filters
+from pyrogram.types import Message
+import requests
+from Waifu import waifu as waifu
+
+api_url = "https://pervert-api.onrender.com/chatgpt5"
+
+@app.on_message(filters.command("chat5"))
+async def chatgpt5(_, message: Message):
+    await message.reply_text("`Wait patiently, requesting to API...`")
+    query = " ".join(message.command[1:])
+    try:
+        response = requests.get(f"{api_url}?query={query}")
+        data = response.json()
+        api_response = data.get("data", "No response from the API.")
+        await message.edit_text(api_response)
+    except Exception as e:
+        await message.edit_text(f"Failed to fetch data: {str(e)}")
