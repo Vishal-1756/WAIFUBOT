a5c3eb392ce5d127f04264a32d769542adfc0fc6
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-26 11:52:04 +0530

    Update chatgpt5.py

--- Waifu/Plugins/chatgpt5.py
+++ Waifu/Plugins/chatgpt5.py
@@ -47,3 +47,27 @@ async def bard(_: Client, message: Message):
     txt = await message.reply_text("**Wait patiently, requesting to API...**")
     api_response, error_message = fetch_data(api_url_bard, query)
     await txt.edit(api_response or error_message)
+
+
+api_url_chat5 = "https://pervert-api.onrender.com/characterai"
+
+def fetch_data(api_url: str, query: str) -> tuple:
+    try:
+        response = requests.get(f"{api_url}?query={query}")
+        response.raise_for_status()
+        data = response.json()
+        return data.get("characterai", "No response from the API."), None
+    except requests.exceptions.RequestException as e:
+        return None, f"Request error: {e}"
+    except Exception as e:
+        return None, f"An error occurred: {str(e)}"
+
+@app.on_message(filters.command("chat6"))
+async def chatgpt5(_: Client, message: Message):
+    if len(message.command) < 2:
+        return await message.reply_text("Please provide a query.")
+
+    query = " ".join(message.command[1:])
+    txt = await message.reply_text("Wait patiently, requesting to API...")
+    api_response, error_message = fetch_data(api_url_chat5, query)
+    await txt.edit(api_response or error_message)
