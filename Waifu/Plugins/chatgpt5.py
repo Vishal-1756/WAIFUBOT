48d21b5e6360e7097d83d1fdcd9538cb10606b84
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-26 10:49:51 +0530

    Update chatgpt5.py

--- Waifu/Plugins/chatgpt5.py
+++ Waifu/Plugins/chatgpt5.py
@@ -25,3 +25,50 @@ async def chatgpt5(_: Client, message: Message):
             await txt.edit(f"HTTP error: {e}")
         except Exception as e:
             await txt.edit(f"An error occurred: {str(e)}")
+
+api_url = "https://pervert-api.onrender.com/chatgpt4"
+
+@app.on_message(filters.command("chat4"))
+async def chatgpt5(_: Client, message: Message):
+    # Check if there is a query provided
+    if len(message.command) < 2:
+        return await message.reply_text("**Please provide a query.**")
+
+    query = " ".join(message.command[1:])
+    txt = await message.reply_text("**Wait patiently, requesting to API...**")
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(f"{api_url}?query={query}")
+            response.raise_for_status()
+            data = response.json()
+            api_response = data.get("ChatGpt5", "No response from the API.")
+            await txt.edit(api_response)
+        except httpx.HTTPError as e:
+            await txt.edit(f"HTTP error: {e}")
+        except Exception as e:
+            await txt.edit(f"An error occurred: {str(e)}")
+
+
+api_url = "https://pervert-api.onrender.com/bardai"
+
+@app.on_message(filters.command("bard"))
+async def chatgpt5(_: Client, message: Message):
+    # Check if there is a query provided
+    if len(message.command) < 2:
+        return await message.reply_text("**Please provide a query.**")
+
+    query = " ".join(message.command[1:])
+    txt = await message.reply_text("**Wait patiently, requesting to API...**")
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(f"{api_url}?query={query}")
+            response.raise_for_status()
+            data = response.json()
+            api_response = data.get("ChatGpt5", "No response from the API.")
+            await txt.edit(api_response)
+        except httpx.HTTPError as e:
+            await txt.edit(f"HTTP error: {e}")
+        except Exception as e:
+            await txt.edit(f"An error occurred: {str(e)}")
