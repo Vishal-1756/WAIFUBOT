af0d53d74514fbbbe037a84cf013781937d70a17
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-26 10:57:24 +0530

    Update chatgpt5.py

--- Waifu/Plugins/chatgpt5.py
+++ Waifu/Plugins/chatgpt5.py
@@ -3,72 +3,48 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 from Waifu import waifu as app
 
-api_url = "https://pervert-api.onrender.com/chatgpt5"
+api_url_chat5 = "https://pervert-api.onrender.com/chatgpt5"
+api_url_chat4 = "https://pervert-api.onrender.com/chatgpt4"
+api_url_bard = "https://pervert-api.onrender.com/bardai"
+
+async def fetch_data(api_url: str, query: str) -> str:
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(f"{api_url}?query={query}")
+            response.raise_for_status()
+            data = response.json()
+            return data.get("data", "No response from the API.")
+        except httpx.HTTPError as e:
+            return f"HTTP error: {e}"
+        except Exception as e:
+            return f"An error occurred: {str(e)}"
 
 @app.on_message(filters.command("chat5"))
 async def chatgpt5(_: Client, message: Message):
-    # Check if there is a query provided
     if len(message.command) < 2:
         return await message.reply_text("**Please provide a query.**")
 
     query = " ".join(message.command[1:])
     txt = await message.reply_text("**Wait patiently, requesting to API...**")
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(f"{api_url}?query={query}")
-            response.raise_for_status()
-            data = response.json()
-            api_response = data.get("ChatGpt5", "No response from the API.")
-            await txt.edit(api_response)
-        except httpx.HTTPError as e:
-            await txt.edit(f"HTTP error: {e}")
-        except Exception as e:
-            await txt.edit(f"An error occurred: {str(e)}")
-
-api_url = "https://pervert-api.onrender.com/chatgpt4"
+    api_response = await fetch_data(api_url_chat5, query)
+    await txt.edit(api_response)
 
 @app.on_message(filters.command("chat4"))
-async def chatgpt5(_: Client, message: Message):
-    # Check if there is a query provided
+async def chatgpt4(_: Client, message: Message):
     if len(message.command) < 2:
         return await message.reply_text("**Please provide a query.**")
 
     query = " ".join(message.command[1:])
     txt = await message.reply_text("**Wait patiently, requesting to API...**")
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(f"{api_url}?query={query}")
-            response.raise_for_status()
-            data = response.json()
-            api_response = data.get("ChatGpt5", "No response from the API.")
-            await txt.edit(api_response)
-        except httpx.HTTPError as e:
-            await txt.edit(f"HTTP error: {e}")
-        except Exception as e:
-            await txt.edit(f"An error occurred: {str(e)}")
-
-
-api_url = "https://pervert-api.onrender.com/bardai"
+    api_response = await fetch_data(api_url_chat4, query)
+    await txt.edit(api_response)
 
 @app.on_message(filters.command("bard"))
-async def chatgpt5(_: Client, message: Message):
-    # Check if there is a query provided
+async def bard(_: Client, message: Message):
     if len(message.command) < 2:
         return await message.reply_text("**Please provide a query.**")
 
     query = " ".join(message.command[1:])
     txt = await message.reply_text("**Wait patiently, requesting to API...**")
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(f"{api_url}?query={query}")
-            response.raise_for_status()
-            data = response.json()
-            api_response = data.get("ChatGpt5", "No response from the API.")
-            await txt.edit(api_response)
-        except httpx.HTTPError as e:
-            await txt.edit(f"HTTP error: {e}")
-        except Exception as e:
-            await txt.edit(f"An error occurred: {str(e)}")
+    api_response = await fetch_data(api_url_bard, query)
+    await txt.edit(api_response)
