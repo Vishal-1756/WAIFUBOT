a82e81d3a02c1b502cad3de13bbc72c5c8a864ad
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-26 10:07:08 +0530

    Update chatgpt5.py

--- chatgpt5.py
+++ chatgpt5.py
@@ -1,26 +1,27 @@
-from pyrogram import filters
+import httpx
+from pyrogram import filters, Client
 from pyrogram.types import Message
-import requests
-from Waifu import waifu as waifu
+from Waifu import waifu as app
 
 api_url = "https://pervert-api.onrender.com/chatgpt5"
 
 @app.on_message(filters.command("chat5"))
-async def chatgpt5(_, message: Message):
-   
+async def chatgpt5(_: Client, message: Message):
+    # Check if there is a query provided
     if len(message.command) < 2:
-        await message.reply_text("`Please provide a query.`")
-        return
+        return await message.reply_text("**Please provide a query.**")
 
-    
     query = " ".join(message.command[1:])
-    
-    await message.reply_text("`Wait patiently, requesting to API...`")
+    txt = await message.reply_text("**Wait patiently, requesting to API...**")
 
-    try:
-        response = requests.get(f"{api_url}?query={query}")
-        data = response.json()
-        api_response = data.get("data", "No response from the API.")
-        await message.edit_text(api_response)
-    except Exception as e:
-        await message.edit_text(f"Failed to fetch data: {str(e)}")
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(f"{api_url}?query={query}")
+            response.raise_for_status()
+            data = response.json()
+            api_response = data.get("ChatGpt5", "No response from the API.")
+            await txt.edit(api_response)
+        except httpx.HTTPError as e:
+            await txt.edit(f"HTTP error: {e}")
+        except Exception as e:
+            await txt.edit(f"An error occurred: {str(e)}")
