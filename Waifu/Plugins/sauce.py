70352bf507853c34af9ca0cfe26d111705cd9210
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-28 10:52:58 +0530

    Update sauce.py

--- Waifu/Plugins/sauce.py
+++ Waifu/Plugins/sauce.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from telegraph import upload_file  # Assuming you have the 'telegraph' library installed
+from telegraph import upload_file
 from Waifu import waifu as app
 
 api_url = "https://api.betabotz.org/api/webzone/whatanime"
@@ -22,49 +22,59 @@ async def shorten_video_url(video_url):
         return None
 
 
+async def get_file_id_from_message(message):
+    file_id = None
+    if message.media:
+        file_id = message.media.file_id
+    return file_id
+
+
 async def sauce_command(client, message):
     try:
         # Check if the message has a quoted message
         if message.reply_to_message and message.reply_to_message.media:
-            media = message.reply_to_message.media
-            file_data = await media.download()
-            telegraph_url = upload_file(file_data)
-            if telegraph_url:
-                api_params = {"query": telegraph_url, "apikey": "GK5zaGhL"}  # Replace with your API key
-                wait_message = await message.reply_text("Please wait... Uploading your query to the API.")
+            file_id = await get_file_id_from_message(message.reply_to_message)
+            if file_id:
+                media_path = await client.download_media(file_id)
+                telegraph_url = upload_file(media_path)
 
-                # Make a request to the whatanime API
-                response = requests.get(api_url, params=api_params)
-                api_data = response.json()
+                if telegraph_url:
+                    api_params = {"query": telegraph_url, "apikey": "GK5zaGhL"}  # Replace with your API key
+                    wait_message = await message.reply_text("Please wait... Uploading your query to the API.")
 
-                # Delete the "Please wait..." message
-                wait_message.delete()
+                    # Make a request to the whatanime API
+                    response = requests.get(api_url, params=api_params)
+                    api_data = response.json()
 
-                if api_data.get("status") and api_data.get("code") == 200:
-                    result_data = api_data["result"]["data"]
-                    name = result_data["filename"].replace('.mp4', '')
-                    episode = result_data["episode"]
-                    image = result_data["image"]
-                    video = result_data["video"]
+                    # Delete the "Please wait..." message
+                    wait_message.delete()
 
-                    # Shorten the video URL
-                    tinyurl = await shorten_video_url(video)
+                    if api_data.get("status") and api_data.get("code") == 200:
+                        result_data = api_data["result"]["data"]
+                        name = result_data["filename"].replace('.mp4', '')
+                        episode = result_data["episode"]
+                        image = result_data["image"]
+                        video = result_data["video"]
 
-                    caption = f"Name: *{name}*\nEpisode: *{episode}*\nVideo: *{tinyurl}*"
+                        # Shorten the video URL
+                        tinyurl = await shorten_video_url(video)
 
-                    try:
-                        # Send the media with the caption
-                        await message.reply_video(video, caption=caption, parse_mode="MarkdownV2")
-                    except Exception as send_error:
-                        await message.reply_text(f"Error while sending video: {send_error}")
+                        caption = f"Name: *{name}*\nEpisode: *{episode}*\nVideo: *{tinyurl}*"
+
+                        try:
+                            # Send the media with the caption
+                            await message.reply_video(video, caption=caption, parse_mode="MarkdownV2")
+                        except Exception as send_error:
+                            await message.reply_text(f"Error while sending video: {send_error}")
+                    else:
+                        await message.reply_text("Error: Unable to fetch data from the whatanime API")
                 else:
-                    await message.reply_text("Error: Unable to fetch data from the whatanime API")
+                    await message.reply_text("Error occurred while creating a direct link.")
             else:
-                await message.reply_text("Error occurred while creating a direct link.")
+                await message.reply_text("Error: Unable to get file ID from the media.")
         else:
             await message.reply_text("Error: Please reply to a media file.")
     except Exception as command_error:
         await message.reply_text(f"Error: {str(command_error)}")
 
 app.on_message(filters.command("sauce", prefixes="/") | filters.text)(sauce_command)
-
