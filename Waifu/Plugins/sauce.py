fcad89b72b10422db7cc3b279f36421ee9327bcd
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-11-28 10:48:28 +0530

    Update sauce.py

--- Waifu/Plugins/sauce.py
+++ Waifu/Plugins/sauce.py
@@ -1,86 +1,70 @@
 from pyrogram import Client, filters
-from pyrogram.types import InputMediaPhoto
 import requests
+from telegraph import upload_file  # Assuming you have the 'telegraph' library installed
 from Waifu import waifu as app
 
 api_url = "https://api.betabotz.org/api/webzone/whatanime"
-telegraph_url = "https://api.telegra.ph/upload"
+tinyurl_api_url = "https://api.erdwpe.com/api/linkshort/tinyurl"
 
 
-async def get_file_id_from_message(msg):
-    file_id = None
-    message = msg.reply_to_message
-    if not message:
-        return
-    if message.document:
-        if int(message.document.file_size) > 3145728:
-            return
-        mime_type = message.document.mime_type
-        if mime_type not in ("image/png", "image/jpeg"):
-            return
-        file_id = message.document.file_id
+async def shorten_video_url(video_url):
+    try:
+        response = requests.get(tinyurl_api_url, params={"link": video_url})
+        response_data = response.json()
 
-    if message.sticker:
-        if message.sticker.is_animated:
-            if not message.sticker.thumbs:
-                return
-            file_id = message.sticker.thumbs[0].file_id
+        if response_data["status"] == True:
+            return response_data["result"]
         else:
-            file_id = message.sticker.file_id
-
-    if message.photo:
-        file_id = message.photo.file_id
-
-    if message.animation:
-        if not message.animation.thumbs:
-            return
-        file_id = message.animation.thumbs[0].file_id
-
-    if message.video:
-        if not message.video.thumbs:
-            return
-        file_id = message.video.thumbs[0].file_id
-    return file_id
+            print("TinyURL API request failed:", response_data)
+            return None
+    except Exception as error:
+        print("Error shortening URL:", error)
+        return None
 
 
-@app.on_message(filters.command("sauce", prefixes="/") | filters.text)
 async def sauce_command(client, message):
-    file_id = await get_file_id_from_message(message)
+    try:
+        # Check if the message has a quoted message
+        if message.reply_to_message and message.reply_to_message.media:
+            media = message.reply_to_message.media
+            file_data = await media.download()
+            telegraph_url = upload_file(file_data)
+            if telegraph_url:
+                api_params = {"query": telegraph_url, "apikey": "GK5zaGhL"}  # Replace with your API key
+                wait_message = await message.reply_text("Please wait... Uploading your query to the API.")
 
-    if file_id:
-        try:
-            # Download the media using file_id
-            media_path = await client.download_media(file_id)
+                # Make a request to the whatanime API
+                response = requests.get(api_url, params=api_params)
+                api_data = response.json()
 
-            # Upload the media to Telegraph
-            files = {"file": ("media", open(media_path, "rb"))}
-            telegraph_response = requests.post(telegraph_url, files=files)
-            telegraph_data = telegraph_response.json()
+                # Delete the "Please wait..." message
+                wait_message.delete()
 
-            # Make a request to the whatanime API
-            api_params = {
-                "query": f"{telegraph_url}/{telegraph_data['src']}",
-                "apikey": "QlyCxRQV"  # Replace with your API key
-            }
+                if api_data.get("status") and api_data.get("code") == 200:
+                    result_data = api_data["result"]["data"]
+                    name = result_data["filename"].replace('.mp4', '')
+                    episode = result_data["episode"]
+                    image = result_data["image"]
+                    video = result_data["video"]
 
-            # Send "Please wait..." message
-            wait_message = await message.reply_text("Please wait... Uploading your query to the API.")
+                    # Shorten the video URL
+                    tinyurl = await shorten_video_url(video)
 
-            api_response = requests.get(api_url, params=api_params)
-            api_data = api_response.json()
+                    caption = f"Name: *{name}*\nEpisode: *{episode}*\nVideo: *{tinyurl}*"
 
-            # Delete the "Please wait..." message
-            wait_message.delete()
-
-            if api_data["status"] and api_data["code"] == 200:
-                result = api_data["result"]["data"]
-                caption = f"Anilist: {result['anilist']}\nFile Name: {result['filename']}\nSimilarity: {result['similarity']}\nVideo Link: {result['video']}\nImage Link: {result['image']}"
-                await message.reply_photo(result['image'], caption=caption)
+                    try:
+                        # Send the media with the caption
+                        await message.reply_video(video, caption=caption, parse_mode="MarkdownV2")
+                    except Exception as send_error:
+                        await message.reply_text(f"Error while sending video: {send_error}")
+                else:
+                    await message.reply_text("Error: Unable to fetch data from the whatanime API")
             else:
-                await message.reply_text("Error: Unable to fetch data from the whatanime API")
+                await message.reply_text("Error occurred while creating a direct link.")
+        else:
+            await message.reply_text("Error: Please reply to a media file.")
+    except Exception as command_error:
+        await message.reply_text(f"Error: {str(command_error)}")
 
-        except Exception as e:
-            await message.reply_text(f"Error: {str(e)}")
-    else:
-        await message.reply_text("Please reply to a photo or video or provide a direct link when using the /sauce command.")
+app.on_message(filters.command("sauce", prefixes="/") | filters.text)(sauce_command)
 
