c6dcb69b6c089c7113f52adaae33cf904819f74e
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-23 13:49:32 +0530

    Update sauce.py

--- Waifu/Plugins/sauce.py
+++ Waifu/Plugins/sauce.py
@@ -22,16 +22,6 @@ async def telegraph(message, path):
 
     return url
 
-def create_buttons(request_url, similar_url):
-    keyboard = [
-        [
-            types.InlineKeyboardButton("Request URL", url=request_url),
-            types.InlineKeyboardButton("Similar URL", url=similar_url),
-            types.InlineKeyboardButton("Bing URL", url=top_one_url)
-        ]
-    ]
-    return types.InlineKeyboardMarkup(keyboard)
-
 @app.on_message(filters.command("r") & filters.reply)
 async def reverse_search(client, message):
     reply_message = message.reply_to_message
@@ -42,7 +32,8 @@ async def reverse_search(client, message):
         url = API_URL.format(url=telegraph_url)
         url2 = API_URL_BING.format(url=telegraph_url)
     elif reply_message.text:
-        url = API_URL.format(url=reply_message.text)
+        telegraph_url = "Your text here"  # Replace with actual telegraph_url for text case
+        url = API_URL.format(url=telegraph_url)
         url2 = API_URL_BING.format(url=telegraph_url)
     else:
         await message.reply_text("Unsupported message type. Reply to an image or provide a URL.")
@@ -50,15 +41,36 @@ async def reverse_search(client, message):
 
     try:
         response = requests.get(url)
-        responce2 = request.get(url2)
+        response2 = requests.get(url2)
         result = response.json()
-        result2 = response2.json()     
+        result2 = response2.json()
         image_description = result["result"]["image"]
-        image_description2 = result2["bestResults"]["name"]      
         request_url = result["result"]["requestUrl"]
-        top_one_url = result2["bestResults"]["url"]
-        similar_url = result["similarUrl"]
-        buttons = create_buttons(request_url, similar_url, top_one_url)
-        await message.reply_text(f"From Google Search Engine: {image_description}"\n\nFrom Bing Serch Engine:\n {image_description}\n\n Api Credits @Awesome_Tofu for Google\n@LexicaAPI For Bing, reply_markup=buttons)
+        similar_urls = result.get("similarUrl", [])
+
+        # Process the top 10 Bing results
+        results2 = result2.get("bestResults", [])
+        image_descriptions2 = [res["name"] for res in results2[:10]]
+        urls2 = [res["url"] for res in results2[:10]]
+        more_results_text = "\n".join([f"{i + 1}. {desc}: {url}" for i, (desc, url) in enumerate(zip(image_descriptions2, urls2))])
+
+        buttons = create_buttons(request_url, similar_urls, more_results_text)
+        text = f"From Google Search Engine: {image_description}\n\nFrom Bing Search Engine:\n"
+        text += "\n".join([f"{i + 1}. {desc}" for i, desc in enumerate(image_descriptions2)])
+
+        await message.reply_text(text, reply_markup=buttons)
     except Exception as e:
         await message.reply_text(f"Error fetching information: {str(e)}")
+
+
+def create_buttons(request_url, similar_urls, more_results_text):
+    keyboard = [
+        [
+            types.InlineKeyboardButton("Request URL", url=request_url),
+            types.InlineKeyboardButton("Similar URLs", url=similar_url) for similar_url in similar_urls
+        ],
+        [
+            types.InlineKeyboardButton("More Results", callback_data=f"more_results:{more_results_text}")
+        ]
+    ]
+    return types.InlineKeyboardMarkup(keyboard)
