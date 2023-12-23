b28b25ef4a43d13a0d1d1de885181d8022e5e0c5
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-23 13:44:11 +0530

    Update sauce.py

--- Waifu/Plugins/sauce.py
+++ Waifu/Plugins/sauce.py
@@ -26,12 +26,13 @@ def create_buttons(request_url, similar_url):
     keyboard = [
         [
             types.InlineKeyboardButton("Request URL", url=request_url),
-            types.InlineKeyboardButton("Similar URL", url=similar_url)
+            types.InlineKeyboardButton("Similar URL", url=similar_url),
+            types.InlineKeyboardButton("Bing URL", url=top_one_url)
         ]
     ]
     return types.InlineKeyboardMarkup(keyboard)
 
-@app.on_message(filters.command("pp") & filters.reply)
+@app.on_message(filters.command("r") & filters.reply)
 async def reverse_search(client, message):
     reply_message = message.reply_to_message
 
@@ -39,19 +40,25 @@ async def reverse_search(client, message):
         photo_path = await reply_message.download()
         telegraph_url = await telegraph(message, photo_path)
         url = API_URL.format(url=telegraph_url)
+        url2 = API_URL_BING.format(url=telegraph_url)
     elif reply_message.text:
         url = API_URL.format(url=reply_message.text)
+        url2 = API_URL_BING.format(url=telegraph_url)
     else:
         await message.reply_text("Unsupported message type. Reply to an image or provide a URL.")
         return
 
     try:
         response = requests.get(url)
+        responce2 = request.get(url2)
         result = response.json()
+        result2 = response2.json()     
         image_description = result["result"]["image"]
+        image_description2 = result2["bestResults"]["name"]      
         request_url = result["result"]["requestUrl"]
+        top_one_url = result2["bestResults"]["url"]
         similar_url = result["similarUrl"]
-        buttons = create_buttons(request_url, similar_url)
-        await message.reply_text(f"Image: {image_description}", reply_markup=buttons)
+        buttons = create_buttons(request_url, similar_url, top_one_url)
+        await message.reply_text(f"From Google Search Engine: {image_description}"\n\nFrom Bing Serch Engine:\n {image_description}\n\n Api Credits @Awesome_Tofu for Google\n@LexicaAPI For Bing, reply_markup=buttons)
     except Exception as e:
         await message.reply_text(f"Error fetching information: {str(e)}")
