0aa0fe253b79b6e792d6cf1189202162883355e5
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-23 14:58:43 +0530

    Update sauce.py

--- Waifu/Plugins/sauce.py
+++ Waifu/Plugins/sauce.py
@@ -3,7 +3,7 @@ from telegraph import upload_file
 from json import JSONDecodeError
 import requests
 from Waifu import waifu as app
-from telegraph import Telegraph, upload_file
+from telegraph import Telegraph
 
 API_URL = "https://reverse-pbq1.onrender.com/reverse?url={url}"
 API_URL_BING = "https://api.qewertyy.me/image-reverse/bing?img_url={url}"
@@ -24,19 +24,19 @@ def upload_text_to_telegraph(text_content):
         return None
 
 async def telegraph(path):
-     try:
-       telegraph = upload_file(path)
-     except JSONDecodeError:        
-             print("Failed To Upload 🚫.")
-             url = False
-             return url
-     try:
-        for file_id in telegraph:
-             url = "https://graph.org/" + file_id
-     except:
-         pass
-     return url
-    
+    try:
+        telegraph_file = upload_file(path)
+    except JSONDecodeError:        
+        print("Failed To Upload 🚫.")
+        url = False
+        return url
+    try:
+        for file_id in telegraph_file:
+            url = "https://graph.org/" + file_id
+    except:
+        pass
+    return telegraph_url
+
 def create_buttons(request_url, similar_url, more_results_text_url):
     keyboard = [
         [
@@ -52,11 +52,11 @@ def create_buttons(request_url, similar_url, more_results_text_url):
 @app.on_message(filters.command("kela") & filters.reply)
 async def reverse_search(client, message):
     reply_message = message.reply_to_message
-     
+    
     if reply_message.photo:
         m = await message.reply_text("`Parsing Your Media Wait`")
         photo_path = await reply_message.download()
-        telegraph_url = upload_to_telegraph(photo_path)
+        telegraph_url = await telegraph(photo_path)
         url = API_URL.format(url=telegraph_url)
         url2 = API_URL_BING.format(url=telegraph_url)            
     else:
@@ -77,7 +77,7 @@ async def reverse_search(client, message):
         more_results_text = "\n".join([f"{i + 1}. {desc}: {url}" for i, (desc, url) in enumerate(zip(image_descriptions2, urls2))])
 
         if telegraph_url:
-            more_results_text_url = upload_to_telegraph(None, more_results_text)
+            more_results_text_url = await upload_text_to_telegraph(more_results_text)
             buttons = create_buttons(request_url, similar_url, more_results_text_url)
             text = f"From Google Search Engine: {image_description}\n\nFrom Bing Search Engine:\n"
             text += "\n".join([f"{i + 1}. {desc}" for i, desc in enumerate(image_descriptions2)])
